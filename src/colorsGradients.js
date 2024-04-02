09d8de654b6bdc8deecd4c72e6e84d61ce7144a4
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-04-02 14:42:32 -0600

    Changed Metadata and order of colors

--- src/colorsGradients.js
+++ src/colorsGradients.js
@@ -2,8 +2,8 @@ const colorsGradients = new Map([
     [0, 'linear-gradient(#fcff58 0%, #fdff89 100%)'], // yellow
     [1, 'linear-gradient(#a1c4fd 0%, #c2e9fb 100%)'], // blue
     [2, 'linear-gradient(#ff758c 0%, #ff7eb9 100%)'], // pink
-    [3, 'linear-gradient(#fda085 0%, #f6d365 100%)'], // orange
-    [4, 'linear-gradient(#b6d7a8 0%, #daf7a1 100%)'], // green
+    [3, 'linear-gradient(#b6d7a8 0%, #daf7a1 100%)'], // green
+    [4, 'linear-gradient(#fda085 0%, #f6d365 100%)'], // orange
 ]);
 
 export default colorsGradients;
